f7e2e19867900c3a8c6e14e1bf9aa68f08e4bf49
Vitor Paiva <paiva.v@icloud.com>
2022-05-12 10:38:13 -0300

    feat :sparkles:(lists): added list divider component

---
+++ src/components/ListDivider/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 90%;
+  height: 1px;
+  align-self: flex-end;
+  background-color: ${({ theme }) => theme.colors.secondary};
+`;
