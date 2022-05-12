2e84fef7bf828d6658f2b69a839de64c5a422049
Vitor Paiva <paiva.v@icloud.com>
2022-05-12 10:26:25 -0300

    feat :sparkles:(api): added api file for connection with database

---
+++ src/api/api.ts
@@ -0,0 +1,9 @@
+//import axios from 'axios';
+
+const axios = require('axios').default;
+
+const api = axios.create({
+  baseURL: 'https://x6if-pd9g-tkt7.n7.xano.io/api:4znihxXN',
+});
+
+export default api;
